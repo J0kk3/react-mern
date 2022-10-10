597ac32320a928b8892a5339639855ff5c6922ba
J0kk3 <joakim_88@msn.com>
2022-10-10 21:56:53 +0200

    [ADD] sideDrawer portal

--- src/shared/components/Navigation/SideDrawer.jsx
+++ src/shared/components/Navigation/SideDrawer.jsx
@@ -3,11 +3,7 @@ import './SideDrawer.css';
 
 const SideDrawer = props =>
 {
-    return (
-        <aside className='side-drawer'>
-            { props.children }
-        </aside>
-    );
+    const content = <aside className='side-drawer'>{ props.children }</aside>;
 };
 
 export default SideDrawer;
