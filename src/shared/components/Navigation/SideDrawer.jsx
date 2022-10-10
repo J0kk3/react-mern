5021f6b1cf97a0694492d47e0355f1a60846bdcd
J0kk3 <joakim_88@msn.com>
2022-10-10 21:52:32 +0200

    [ADD] SideDrawer

---
+++ src/shared/components/Navigation/SideDrawer.jsx
@@ -0,0 +1,13 @@
+//styles
+import './SideDrawer.css';
+
+const SideDrawer = props =>
+{
+    return (
+        <aside className='side-drawer'>
+            { props.children }
+        </aside>
+    );
+};
+
+export default SideDrawer;
