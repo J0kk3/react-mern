64ceda59210feb00652142051e2e10d1dba86af4
J0kk3 <joakim_88@msn.com>
2022-10-10 22:17:01 +0200

    [ADD] drawer backdrop, drawer state & drawer animation

--- src/shared/components/Navigation/MainNavigation.jsx
+++ src/shared/components/Navigation/MainNavigation.jsx
@@ -1,22 +1,38 @@
 import { Link } from "react-router-dom";
+//hooks
+import { useState } from "react";
 //Components
 import MainHeader from "./MainHeader";
 import NavLinks from "./NavLinks";
 import SideDrawer from "./SideDrawer";
+import Backdrop from "../UIElements/Backdrop";
 //Styles
 import './MainNavigation.css';
 
 const MainNavigation = props =>
 {
+    const [ drawerIsOpen, setDrawerIsOpen ] = useState( false );
+
+    const openDrawerHandler = () =>
+    {
+        setDrawerIsOpen( true );
+    };
+
+    const closeDrawerHandler = () =>
+    {
+        setDrawerIsOpen( false );
+    };
+
     return (
         <>
-            <SideDrawer>
+            { drawerIsOpen && <Backdrop onClick={ closeDrawerHandler } /> }
+            <SideDrawer show={ drawerIsOpen } onClick={ closeDrawerHandler }>
                 <nav className="main-navigation__drawer-nav">
                     <NavLinks />
                 </nav>
             </SideDrawer>
             <MainHeader>
-                <button className="main-navigation__menu-btn">
+                <button className="main-navigation__menu-btn" onClick={ openDrawerHandler }>
                     <span />
                     <span />
                     <span />
