c343e8ac1b473d99ecf8329941f112a4a5d60173
J0kk3 <joakim_88@msn.com>
2022-10-20 20:42:57 +0200

    [BUG] correct reference in delete button

--- src/places/components/PlaceItem.jsx
+++ src/places/components/PlaceItem.jsx
@@ -63,7 +63,7 @@ const PlaceItem = props =>
                     <div className="place-item__actions">
                         <Button inverse onClick={ openMapHandler }>VIEW ON MAP</Button>
                         <Button to={ `/places/${ props.id }` }>EDIT</Button>
-                        <Button danger onClick={ showConfirmModal }>DELETE</Button>
+                        <Button danger onClick={ showDeleteWarningHandler }>DELETE</Button>
                     </div>
                 </Card>
             </li>
