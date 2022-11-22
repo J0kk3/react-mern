a158427d22cec31779c06c93a06fecf58ab27a46
J0kk3 <joakim_88@msn.com>
2022-11-22 21:36:57 +0100

    [ADD] add places (POST)

--- src/places/pages/NewPlace.jsx
+++ src/places/pages/NewPlace.jsx
@@ -1,6 +1,12 @@
 //hooks
+import { useNavigate } from 'react-router-dom';
+import { useContext } from 'react';
 import { useForm } from '../../shared/hooks/form-hook';
+import { useHttpClient } from '../../shared/hooks/http-hook';
 //components
+import ErrorModal from '../../shared/components/UIElements/ErrorModal';
+import LoadingSpinner from '../../shared/components/UIElements/LoadingSpinner';
+import { AuthContext } from '../../shared/context/auth-context';
 import Input from "../../shared/components/FormElements/Input";
 import Button from "../../shared/components/FormElements/Button";
 import { VALIDATOR_REQUIRE, VALIDATOR_MINLENGTH } from "../../shared/util/validators";
@@ -9,6 +15,8 @@ import "./PlaceForm.css";
 
 const NewPlace = () =>
 {
+  const auth = useContext( AuthContext );
+  const { isLoading, error, sendRequest, clearError } = useHttpClient();
   const [ formState, inputHandler ] = useForm( {
     title: {
       value: "",
@@ -26,41 +34,64 @@ const NewPlace = () =>
     false
   );
 
-  const placeSubmitHandler = event =>
+  const history = useNavigate();
+
+  const placeSubmitHandler = async event =>
   {
     event.preventDefault();
-    console.log( formState.inputs ); //send this to the backend
+    try
+    {
+      await sendRequest( "http://localhost:5000/api/places", "POST", JSON.stringify(
+        {
+          title: formState.inputs.title.value,
+          description: formState.inputs.description.value,
+          adress: formState.inputs.adress.value,
+          creator: auth.userId
+        } ),
+        { "Content-Type": "application/json" }
+      );
+      history.push( "/" );
+    }
+    catch ( err )
+    {
+
+    }
   };
 
   return (
-    <form className="place-form" onSubmit={ placeSubmitHandler }>
-      <Input
-        id="title"
-        element="input"
-        type="text"
-        label="Title"
-        validators={ [ VALIDATOR_REQUIRE() ] }
-        errorText="Please enter a valid title"
-        onInput={ inputHandler }
-      />
-      <Input
-        id="description"
-        element="textarea"
-        label="Description"
-        validators={ [ VALIDATOR_MINLENGTH( 5 ) ] }
-        errorText="Please enter a valid description (at least 5 characters)."
-        onInput={ inputHandler }
-      />
-      <Input
-        id="adress"
-        element="input"
-        label="Adress"
-        validators={ [ VALIDATOR_REQUIRE() ] }
-        errorText="Please enter a valid adress."
-        onInput={ inputHandler }
-      />
-      <Button type="submit" disabled={ !formState.isValid }>ADD PLACE</Button>
-    </form> );
+    <>
+      <ErrorModal error={ error } onClear={ clearError } />
+      <form className="place-form" onSubmit={ placeSubmitHandler }>
+        { isLoading && <LoadingSpinner asOverlay /> }
+        <Input
+          id="title"
+          element="input"
+          type="text"
+          label="Title"
+          validators={ [ VALIDATOR_REQUIRE() ] }
+          errorText="Please enter a valid title"
+          onInput={ inputHandler }
+        />
+        <Input
+          id="description"
+          element="textarea"
+          label="Description"
+          validators={ [ VALIDATOR_MINLENGTH( 5 ) ] }
+          errorText="Please enter a valid description (at least 5 characters)."
+          onInput={ inputHandler }
+        />
+        <Input
+          id="adress"
+          element="input"
+          label="Adress"
+          validators={ [ VALIDATOR_REQUIRE() ] }
+          errorText="Please enter a valid adress."
+          onInput={ inputHandler }
+        />
+        <Button type="submit" disabled={ !formState.isValid }>ADD PLACE</Button>
+      </form>
+    </>
+  );
 };
 
 export default NewPlace;
--- src/user/pages/Auth.jsx
+++ src/user/pages/Auth.jsx
@@ -61,7 +61,7 @@ const Auth = () =>
         {
             try
             {
-                await sendRequest( "http://localhost:5000/api/users/login", "POST", JSON.stringify(
+                const responseData = await sendRequest( "http://localhost:5000/api/users/login", "POST", JSON.stringify(
                     {
                         email: formState.inputs.email.value,
                         password: formState.inputs.password.value
@@ -70,7 +70,7 @@ const Auth = () =>
                         "Content-Type": "application/json"
                     },
                 );
-                auth.login();
+                auth.login( responseData.user.id );
             }
             catch ( err )
             {
@@ -81,7 +81,7 @@ const Auth = () =>
         {
             try
             {
-                await sendRequest( "http://localhost:5000/api/users/signup", "POST", JSON.stringify(
+                const responseData = await sendRequest( "http://localhost:5000/api/users/signup", "POST", JSON.stringify(
                     {
                         name: formState.inputs.name.value,
                         email: formState.inputs.email.value,
@@ -91,7 +91,7 @@ const Auth = () =>
                         "Content-Type": "application/json"
                     } );
 
-                auth.login();
+                auth.login( responseData.user.id );
             } catch ( err )
             {
 
