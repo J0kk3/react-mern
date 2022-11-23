9fc6aa9b269354d8e64be07d3b39c0b2695a80f9
J0kk3 <joakim_88@msn.com>
2022-11-23 23:37:58 +0100

    [REFACTOR] moved auth logic into a seperate auth-hook

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,6 @@ import
   Routes,
   Redirect,
 } from 'react-router-dom';
-//hooks
-import { useState, useCallback, useEffect } from 'react';
 //pages
 import Auth from './user/pages/Auth';
 import Users from './user/pages/Users';
@@ -14,55 +12,13 @@ import UserPlaces from './places/pages/UserPlaces';
 import NewPlace from './places/pages/NewPlace';
 import UpdatePlace from './places/pages/UpdatePlace';
 //components
+import { useAuth } from './shared/hooks/auth-hook';
 import MainNavigation from './shared/components/Navigation/MainNavigation';
 import { AuthContext } from './shared/context/auth-context';
 
-let logoutTimer;
-
 const App = () =>
 {
-  const [ token, setToken ] = useState( false );
-  const [ tokenExpirationDate, setTokenExpirationDate ] = useState();
-  const [ userId, setUserId ] = useState( false );
-
-  const login = useCallback( ( uid, token, expirationDate ) =>
-  {
-    setToken( token );
-    setUserId( uid );
-    const tokenExpirationDate = expirationDate || new Date( new Date().getTime() + 1000 * 60 * 60 );
-    setTokenExpirationDate( tokenExpirationDate );
-    localStorage.setItem( 'userData', JSON.stringify( { userId: uid, token: token, expiration: tokenExpirationDate.toISOString() } ) );
-  }, [] );
-
-  const logout = useCallback( () =>
-  {
-    setToken( null );
-    setTokenExpirationDate( null );
-    setUserId( null );
-    localStorage.removeItem( 'userData' );
-  }, [] );
-
-  useEffect( () =>
-  {
-    if ( token && tokenExpirationDate )
-    {
-      const remainingTime = tokenExpirationDate.getTime() - new Date().getTime();
-      logoutTimer = setTimeout( logout, remainingTime );
-    } else
-    {
-      clearTimeout( logoutTimer );
-    }
-  }, [ token, logout, tokenExpirationDate ] );
-
-  useEffect( () =>
-  {
-    const storedData = JSON.parse( localStorage.getItem( 'userData' ) );
-    // if the left date is greater than the right, it means its' expiration date is still valid
-    if ( storedData && storedData.token && new Date( storedData.expiration ) > new Date() )
-    {
-      login( storedData.userId, storedData.token, new Date( storedData.expiration ) );
-    }
-  }, [ login ] );
+  const { token, login, logout, userId } = useAuth();
 
   let routes;
 
