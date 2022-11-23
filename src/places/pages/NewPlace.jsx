d8b1565db193dfd2c8a63befdedb454f9cbb6eda
J0kk3 <joakim_88@msn.com>
2022-11-23 23:05:20 +0100

    [ADD] jwt for update & delete

--- src/places/pages/NewPlace.jsx
+++ src/places/pages/NewPlace.jsx
@@ -56,7 +56,10 @@ const NewPlace = () =>
       formData.append( 'adress', formState.inputs.adress.value );
       formData.append( 'creator', auth.userId );
       formData.append( 'image', formState.inputs.image.value );
-      await sendRequest( "http://localhost:5000/api/places", "POST", formData );
+      await sendRequest( "http://localhost:5000/api/places", "POST", formData,
+        {
+          Authorization: 'Bearer ' + auth.token
+        } );
       history.push( "/" );
     }
     catch ( err ) { }
--- src/user/pages/Auth.jsx
+++ src/user/pages/Auth.jsx
@@ -80,7 +80,7 @@ const Auth = () =>
                         "Content-Type": "application/json"
                     },
                 );
-                auth.login( responseData.user.id );
+                auth.login( responseData.userId, responseData.token );
             }
             catch ( err ) { }
         }
@@ -97,7 +97,7 @@ const Auth = () =>
                     "POST",
                     formData
                 );
-                auth.login( responseData.user.id );
+                auth.login( responseData.userId, responseData.token );
             } catch ( err )
             {
 
