da81a755a71ac01153dba5def1f0a938cca6618c
J0kk3 <joakim_88@msn.com>
2022-11-23 23:33:01 +0100

    [ADD] auto-login & auto-logout

--- src/App.jsx
+++ src/App.jsx
@@ -17,9 +17,12 @@ import UpdatePlace from './places/pages/UpdatePlace';
 import MainNavigation from './shared/components/Navigation/MainNavigation';
 import { AuthContext } from './shared/context/auth-context';
 
+let logoutTimer;
+
 const App = () =>
 {
   const [ token, setToken ] = useState( false );
+  const [ tokenExpirationDate, setTokenExpirationDate ] = useState();
   const [ userId, setUserId ] = useState( false );
 
   const login = useCallback( ( uid, token, expirationDate ) =>
@@ -27,15 +30,29 @@ const App = () =>
     setToken( token );
     setUserId( uid );
     const tokenExpirationDate = expirationDate || new Date( new Date().getTime() + 1000 * 60 * 60 );
+    setTokenExpirationDate( tokenExpirationDate );
     localStorage.setItem( 'userData', JSON.stringify( { userId: uid, token: token, expiration: tokenExpirationDate.toISOString() } ) );
   }, [] );
 
   const logout = useCallback( () =>
   {
     setToken( null );
+    setTokenExpirationDate( null );
     setUserId( null );
     localStorage.removeItem( 'userData' );
   }, [] );
+
+  useEffect( () =>
+  {
+    if ( token && tokenExpirationDate )
+    {
+      const remainingTime = tokenExpirationDate.getTime() - new Date().getTime();
+      logoutTimer = setTimeout( logout, remainingTime );
+    } else
+    {
+      clearTimeout( logoutTimer );
+    }
+  }, [ token, logout, tokenExpirationDate ] );
 
   useEffect( () =>
   {
